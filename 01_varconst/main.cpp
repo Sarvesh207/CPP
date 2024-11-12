de0741fd0e8441e23e2ca534bd2e89d159bf77d1
Sarvesh207 <sarveshgaynar@gmail.com>
2024-11-12 23:43:21 +0530

    initial commit

---
+++ 01_varconst/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Welcome to chai with cpp 1" << endl;
+    cout << "Welcome to chai with cpp 1" << endl;
+    cout << "Welcome to chai with cpp 3" << endl;
+    return 0;
+}
