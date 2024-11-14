2e8feb812b4e0abba4d3d3d627f176eeb1a9f9e8
Sarvesh207 <sarveshgaynar@gmail.com>
2024-11-14 21:58:46 +0530

    cpp conditionals

---
+++ oneShotCPP/conditionals/conditionals.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// write a program that taks an input of age
+//  and prints if uou are adult or not
+
+int main()
+{
+    int age;
+    cin >> age;
+    if (age >= 18)
+    {
+        cout << "You are an adult";
+    }
+
+    cout << "You are not an adult !";
+
+    return 0;
+}
