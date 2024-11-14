4304888d81f05abb7263160c40c01b6a04a015eb
Sarvesh207 <sarveshgaynar@gmail.com>
2024-11-14 07:48:36 +0530

    cpp skeleton code

---
+++ 02_primitives/modifiers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // int smallTeaPack = 100;
+
+    
+
+    cout << smallTeaPack << endl;
+
+    return 0;
+}
---
+++ oneShotCPP/learnC++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cout << "Give me values of x" << endl;
+    cin >> x;
+    cout << "values of x is "  << x ;
+
+    return 0;
+}
