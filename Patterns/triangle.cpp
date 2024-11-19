781446fe3197fdc698368dee4f9252702d0200f9
Sarvesh207 <sarveshgaynar@gmail.com>
2024-11-19 22:37:10 +0530

    star tringle

--- Patterns/triangle.cpp
+++ Patterns/triangle.cpp
@@ -20,6 +20,28 @@ void nNumberTriangle(int n) {
     }
 }
 
+//  star taingle 
+void nStarTriangle(int n) {
+    // Write your code here.
+    for(int i=0; i<n; i++){
+        // space
+        for(int j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+
+        //  star
+        for(int k=0; k<2*i+1; k++){
+            cout << "*";
+        }
+
+        //  space
+        for(int j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main(){
     pattern6(4);
 }
