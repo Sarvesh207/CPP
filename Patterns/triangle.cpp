e4fbd30ce5790b779fe375d17049b130bfc0b4d3
Sarvesh207 <sarveshgaynar@gmail.com>
2024-11-19 08:42:51 +0530

    nNumberTriangle

--- Patterns/triangle.cpp
+++ Patterns/triangle.cpp
@@ -10,6 +10,16 @@ void printPattern(int n){
         cout<< endl;
     }
 }
+void nNumberTriangle(int n) {
+    // Write your code here.
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n-i+1; j++){
+            cout << j<< " ";
+        }
+        cout << endl;
+    }
+}
+
 int main(){
-    printPattern(4);
+    pattern6(4);
 }
