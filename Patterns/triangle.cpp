e110411c42c6b693f1c4d5550ed0a30333df6cd6
Sarvesh207 <sarveshgaynar@gmail.com>
2024-11-20 08:56:24 +0530

    nStarTriangle2

--- Patterns/triangle.cpp
+++ Patterns/triangle.cpp
@@ -42,6 +42,37 @@ void nStarTriangle(int n) {
     }
 }
 
+
+void nStarTriangle2(int n) {
+    // Write your code here.
+    for (int i = 0; i < n; i++)
+    {
+        // For printing the spaces before stars in each row
+        for (int j =0; j<i; j++)
+        {
+            cout <<" ";
+        }
+       
+        // For printing the stars in each row
+        for(int j=0;j< 2*n -(2*i +1);j++){
+            
+            cout<<"*";
+        }
+        
+        // For printing the spaces after the stars in each row
+         for (int j =0; j<i; j++)
+        {
+            cout <<" ";
+        }
+        
+        // As soon as the stars for each iteration are printed, we move to the
+        // next row and give a line break otherwise all stars
+        // would get printed in 1 line.
+        cout << endl;
+    }
+
+}
+
 int main(){
     pattern6(4);
 }
