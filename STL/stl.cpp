8b20fae75eee7e7b3dc64e4fae5fe782bb101b98
Sarvesh207 <sarveshgaynar@gmail.com>
2024-11-20 16:24:19 +0530

    stl complete

--- STL/stl.cpp
+++ STL/stl.cpp
@@ -27,6 +27,47 @@ void explainPair()
     cout << arr[1];
 }
 
+void explainVactor(){
+    vector<int> v;
+
+    // Adding elements
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+
+    // Displaying elements
+    for (int x : v) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    // Accessing and modifying elements
+    v[1] = 50;
+    cout << "Second element: " << v.at(1) << endl;
+
+    // Size and capacity
+    cout << "Size: " << v.size() << ", Capacity: " << v.capacity() << endl;
+
+    // Removing elements
+    v.pop_back();
+    cout << "After pop_back: ";
+    for (int x : v) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+
+}
+
+
+void explainList(){
+    list<int> ls;
+    ls.push_back(2); //{2}
+    ls.emplace_back(4); //{2,4}
+    ls.push_front(5); //{5,2,4}
+    ls.emplace_front(); // {2,4}
+}
+
 int main()
 {   
     return 0;
