b45193b3cbff3e31b299daf79293b0910d02a244
Sarvesh207 <sarveshgaynar@gmail.com>
2024-11-20 09:16:06 +0530

    starDiamond pattern

---
+++ Patterns/diamond.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+void nStarTriangleUpper(int n) {
+    // Write your code here.
+    for(int i=0; i<n; i++){
+        // space
+        for(int j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+
+        //  star
+        for(int k=0; k<2*i+1; k++){
+            cout << "*";
+        }
+
+        //  space
+        for(int j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+void nStarTriangleLower(int n) {
+    // Write your code here.
+    for(int i=0; i<n; i++){
+        // space
+        for(int j=0; j<i; j++){
+            cout << " ";
+        }
+
+        //  star
+        for(int k=0; k<2*n-(2*i+1); k++){
+            cout << "*";
+        }
+
+        //  space
+        for(int j=0; j<i; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+void nStarDiamond(int n) {
+    // Write your code here.
+    nStarTriangleUpper(n);
+    nStarTriangleLower(n);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    nStarDiamond(n)
+    return 0;
+}
