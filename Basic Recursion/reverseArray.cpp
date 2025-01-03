16e719a79ce7a7f017ca3fbe34f74a6efca80a9c
Sarvesh207 <sarveshgaynar@gmail.com>
2025-01-03 08:55:05 +0530

    check string is palindrome or not

--- Basic Recursion/reverseArray.cpp
+++ Basic Recursion/reverseArray.cpp
@@ -2,21 +2,25 @@
 
 using namespace std;
 
-void f(int i, int arr[],int n){
-    if(i >= n / 2) return;
+bool f(int i, string &s)
+{
+    if (i >= s.size() / 2)
+    {
+        return true;
+    }
 
-    swap(arr[i], arr[n-i-1]);
-    f(i+1,arr, n);
+    if (s[i] !== s[s.size() - i - 1])
+    {
+        return false;
+    }
+
+    return f(i + 1, s);
 }
 
 int main()
 {
-    int n;
-    cin >> n;
-    int arr[n];
-    for(int i=0; i<n; i++) cin >> arr[i];
+    string s = "madam";
+    cout << f(0, s);
 
-    f(0, arr, n);
-    for(int i=0; i<n; i++) cout << arr[i] << " ";
     return 0;
 }
