6fd334ca4a80b05f0704967a7838334470d981dd
Sarvesh207 <sarveshgaynar@gmail.com>
2025-01-06 22:04:49 +0530

    hashmap maps

--- Hashing/StringsFrequnecy.cpp
+++ Hashing/StringsFrequnecy.cpp
@@ -4,28 +4,39 @@ using namespace std;
 
 int main()
 {
-    string s;
+    int n;
 
-    cin >> s;
+    cin >> n;
+    int arr[n];
 
-    // precompute
-
-    int hash[26] = {0};
-
-    for (int i = 0; i < s.size(); i++)
+    for (int i = 0; i < n; i++)
     {
-        hash[s[i] - 'a']++;
+        cin >> arr[i];
     }
+
+    //  precompute
+    map<int, int> map;
+
+    for (int i = 0; i < n; i++)
+    {
+        map[arr[i]]++;
+    }
+
+    //  iterator
+    // for (auto it : map)
+    // {
+    //     cout << it.first << "->" << it.second << endl;
+    // }
 
     int q;
     cin >> q;
     while (q--)
     {
-        char c;
+        int number;
 
-        cin >> c;
+        cin >> number;
         // fetch
-        cout << hash[c - 'a'] << endl;
+        cout << map[number] << endl;
     }
 
     return 0;
