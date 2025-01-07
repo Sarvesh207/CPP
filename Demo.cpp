8389e0aece2ada0514d9190811a6586541197bb4
Sarvesh207 <sarveshgaynar@gmail.com>
2025-01-07 09:14:18 +0530

    optimal freq count with unordered map

--- Demo.cpp
+++ Demo.cpp
@@ -4,30 +4,46 @@ using namespace std;
 
 void count_freq(int arr[], int n)
 {
+    //  Bruteforce
+    // vector<bool> visited(n, false);
 
-    vector<bool> visited(n, false);
+    // for (int i = 0; i < n; i++)
+    // {
+    //     int count = 1;
+
+    //     if (visited[i])
+    //     {
+    //         continue;
+    //     }
+
+    //     visited[i] = true;
+
+    //     for (int j = i + 1; j < n; j++)
+    //     {
+    //         if (arr[i] == arr[j])
+    //         {
+    //             visited[j] = true;
+    //             count++;
+    //         }
+    //     }
+
+    //     cout << arr[i] << " " << count << endl;
+    // }
+
+    //  Optimal Way
+
+    unordered_map<int, int> map;
 
     for (int i = 0; i < n; i++)
     {
-        int count = 1;
+        map[arr[i]]++;
+    }
 
-        if (visited[i])
-        {
-            continue;
-        }
+    //  iterator
 
-        visited[i] = true;
-
-        for (int j = i + 1; j < n; j++)
-        {
-            if (arr[i] == arr[j])
-            {
-                visited[j] = true;
-                count++;
-            }
-        }
-
-        cout << arr[i] << " " << count << endl;
+    for (auto &pair : map)
+    {
+        cout << pair.first << " " << pair.second << endl;
     }
 }
 
