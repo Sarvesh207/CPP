87181247ea3eaf4a9477d9313fa251f58f0817c7
Sarvesh207 <sarveshgaynar@gmail.com>
2025-01-09 08:57:34 +0530

    bubble sort

--- Sorting/selectionSort.cpp
+++ Sorting/selectionSort.cpp
@@ -8,7 +8,7 @@ void selection_sort(int arr[], int n)
     {
         int mini = i;
 
-        for (int j = i ; j <= n - 1; j++)
+        for (int j = i; j <= n - 1; j++)
         {
             if (arr[j] < arr[mini])
             {
@@ -19,6 +19,22 @@ void selection_sort(int arr[], int n)
         int temp = arr[mini];
         arr[mini] = arr[i];
         arr[i] = temp;
+    }
+}
+
+void bubble_sort(int arr[], int n)
+{
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int j = 0; j <= i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+        }
     }
 }
 
@@ -34,7 +50,8 @@ int main()
         cin >> arr[i];
     }
 
-    selection_sort(arr, n);
+    // selection_sort(arr, n);
+    bubble_sort(arr, n);
 
     for (int i = 0; i < n; i++)
     {
