6f78bfacedfbccd11a45ca0ca46455a68773693b
Sarvesh207 <sarveshgaynar@gmail.com>
2025-01-15 08:37:31 +0530

    recursive bubble sort

---
+++ Sorting/recursiveSorting.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void recursiveBubbleSort(int arr[], int n)
+{
+    if (n == 1 )
+        return;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            swap(arr[i], arr[i + 1]);
+        }
+    }
+    recursiveBubbleSort(arr, n - 1);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    recursiveBubbleSort(arr, n);
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
