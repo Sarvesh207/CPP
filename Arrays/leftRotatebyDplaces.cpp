b4d6b825e59f7dabdbfcdd8e74d1c05f056164b7
Sarvesh207 <sarveshgaynar@gmail.com>
2025-01-30 22:00:24 +0530

    optimal

--- Arrays/leftRotatebyDplaces.cpp
+++ Arrays/leftRotatebyDplaces.cpp
@@ -1,24 +1,43 @@
+void reverseArray(vector<int>&arr, int left, int right){
+    while(left <= right){
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
+        right--;
+    }
+}
 vector<int> rotateArray(vector<int>arr, int k) {
     int n = arr.size();
-    int temp [k];
 
-    //  copy first k element into temp array
+    k = k%n;
 
-    for(int i=0; i<k; i++){
-        temp[i] = arr[i];
-    }
+    //  Brute Force
+    // int temp [k];
 
-    //  shift remaining element by i - d places
-    for(int i = k; i<n; i++){
-        arr[i-k] = arr[i];
-    }
+    // //  copy first k element into temp array
 
-    //  copy temp to original array 
-    int j = 0;
-    for(int i =n- k; i<arr.size(); i++){
-        arr[i] = temp[j];
-        j++;
-    }
+    // for(int i=0; i<k; i++){
+    //     temp[i] = arr[i];
+    // }
+
+    // //  shift remaining element by i - d places
+    // for(int i = k; i<n; i++){
+    //     arr[i-k] = arr[i];
+    // }
+
+    // //  copy temp to original array 
+    
+    // for(int i =n- k; i<arr.size(); i++){
+    //     arr[i] = temp[i-n+k];
+    
+    // }
+
+    // Optimal
+
+    reverseArray(arr, 0, k-1);
+    reverseArray(arr, k, n-1);
+    reverseArray(arr, 0, n-1);
 
     return arr;
 }
