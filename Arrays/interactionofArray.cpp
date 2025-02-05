9789dfc083b52aa276304895501b5f8529a34d6e
Sarvesh207 <sarveshgaynar@gmail.com>
2025-02-05 21:45:18 +0530

    optimal and Brute force approch

--- Arrays/interactionofArray.cpp
+++ Arrays/interactionofArray.cpp
@@ -1,22 +1,37 @@
 #include <bits/stdc++.h> 
-//  Brute Force
 vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m)
 {
 	
 
         vector<int> ans;
-        int vis[m] = {0};
+        int i=0;
+        int j=0;
+        // int vis[m] = {0};
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<m; j++){
 
-                if(arr1[i] == arr2[j] && vis[j] == 0){
-                    ans.push_back(arr1[i]);
-                    vis[j] = 1;
-                    break;
-                }
+        //         if(arr1[i] == arr2[j] && vis[j] == 0){
+        //             ans.push_back(arr1[i]);
+        //             vis[j] = 1;
+        //             break;
+        //         }
 
-                if(arr2[j] > arr1[i]) break;
+        //         if(arr2[j] > arr1[i]) break;
+        //     }
+        // }
+
+        //  Optimal Approch 
+
+        while(i <n && j < m){
+            if(arr1[i] < arr2[j]){
+                i++;
+            } else if(arr2[j] < arr1[i]){
+                j++;
+            } else {
+                ans.push_back(arr1[i]);
+                i++;
+                j++;
             }
         }
 
