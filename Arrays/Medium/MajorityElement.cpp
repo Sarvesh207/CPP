c474eb831fce6001c572c7872349110c85222c45
Sarvesh207 <sarveshgaynar@gmail.com>
2025-02-26 08:26:22 +0530

    optimal approch

--- Arrays/Medium/MajorityElement.cpp
+++ Arrays/Medium/MajorityElement.cpp
@@ -51,3 +51,46 @@ public:
         return -1;
     }
 };
+
+//  Optimal
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        int n = nums.size();
+        int cnt = 0;
+        int el = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (cnt == 0)
+            {
+                el = nums[i];
+            }
+            else if (nums[i] == el)
+            {
+                cnt++;
+            }
+            else
+            {
+                cnt--;
+            }
+        }
+
+        int cnt1 = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (el == nums[i])
+            {
+                cnt1++;
+            }
+        }
+
+        if (cnt1 > n / 2)
+        {
+            return el;
+        }
+
+        return -1;
+    }
+};
